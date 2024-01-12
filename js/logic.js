3b7677304b739e448be8868dcaff02e16c3ef6e1
Abtinz <abtinzandi@gmail.com>
2024-01-12 12:01:52 +0330

    SAVE EVENT

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,3 @@
-
-
 //DOM content load for loading elements from html
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -24,23 +22,50 @@ document.addEventListener('DOMContentLoaded', function () {
     const genderHeader = document.querySelector("#gender-header");
     const genderPrediction = document.querySelector("#gender-prediction");
 
-    //error detection will be used for api callings or ...
-    const error = document.querySelector("#error");
-
     let fullname ;
 
     //this event handler will help us to mange clear events
     clear.addEventListener("click", () => {
+
         if (localStorage.getItem(fullname)) {
           localStorage.removeItem(fullname);
           fullname = ""
-          savedAnswer.innerHTML = ""
+          savedAnswer.innerHTML = "Saved Submits Area"
           alert("data is cleared");
         }else{
             alert("data is already cleared");
         }
     
       });
+    
+      //this event handler will help us to mange save events
+      saveButton.addEventListener("click", () => {
+        if (userInputtedFormData.gender !== undefined && userInputtedFormData.name !== '') {
+            if(userInputtedFormData.name.length < 255){
+                var english = /^[A-Za-z ]*$/;
+                var isCorrect = true
+                for (i=0;i<userInputtedFormData.name.length;i++) {
+                    if (!english.test(userInputtedFormData.name[i]) ){
+                        isCorrect = false
+                        break; 
+                    }
+                }
+
+                if(isCorrect){
+                    localStorage.setItem(userInputtedFormData.name, userInputtedFormData.gender);
+                }else{
+                    alert("sorry our predictor wont other alphabet characters");
+                }
+        
+            }else{
+                alert("maximum limitation for inputted name is 255 character");
+            }
+          
+        }else {
+            alert("complete form");
+        }
+      });
+    
     
     
 });
