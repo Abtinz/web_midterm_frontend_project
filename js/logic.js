73763541d171e26cf9cf9ad32c89dcc545e7286e
Abtinz <abtinzandi@gmail.com>
2024-01-12 11:46:14 +0330

    clear.addEventListener

--- js/logic.js
+++ js/logic.js
@@ -27,7 +27,20 @@ document.addEventListener('DOMContentLoaded', function () {
     //error detection will be used for api callings or ...
     const error = document.querySelector("#error");
 
+    let fullname ;
+
+    //this event handler will help us to mange clear events
+    clear.addEventListener("click", () => {
+        if (localStorage.getItem(fullname)) {
+          localStorage.removeItem(fullname);
+          fullname = ""
+          savedAnswer.innerHTML = ""
+          alert("data is cleared");
+        }else{
+            alert("data is already cleared");
+        }
     
+      });
     
     
 });
