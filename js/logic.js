3179a83d65875f2e74e28c8c784b32a619166125
Abtinz <abtinzandi@gmail.com>
2024-01-12 11:41:36 +0330

    values

--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,33 @@
-const PREDICTOR_API_URL = "https://api.genderize.io/?name=";
+
+
+//DOM content load for loading elements from html
+document.addEventListener('DOMContentLoaded', function () {
+
+    //lets define our api url(like kotlin retrofit XD)
+    const PREDICTOR_API_URL = "https://api.genderize.io/?name=";
+
+    const userInputtedFormData = {
+        name: "",
+        gender: undefined,
+    };
+    
+    // inputs values 
+    const nameInput = document.querySelector("#name");
+    const form = document.querySelector("#form");
+    const savedAnswer = document.querySelector("#saved-answer");
+
+    //buttons initializing
+    const saveButton = document.querySelector("#save");
+    const clearButton = document.querySelector("#clear");
+
+    //results of api callings
+    const genderHeader = document.querySelector("#gender-header");
+    const genderPrediction = document.querySelector("#gender-prediction");
+
+    //error detection will be used for api callings or ...
+    const error = document.querySelector("#error");
+
+    
+    
+    
+});
